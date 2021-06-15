636faf643761a4c1cf842b6e3b7f4c5f90e71364
maxim13 <ZLf7XV6s13>
2021-06-15 22:42:20 +0300

    Initial commit

---
+++ src/main/java/ru/sysout/jwt/security/JWTFilter.java
@@ -0,0 +1,59 @@
+package ru.sysout.jwt.security;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import ru.sysout.jwt.service.CustomUserDetailsService;
+
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+
+  @Autowired
+  private JWTUtil jwtUtil;
+
+  @Autowired
+  CustomUserDetailsService userDetailsService;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+      throws ServletException, IOException {
+
+    final String authorizationHeader = request.getHeader("Authorization");
+
+    String username = null;
+    String jwt = null;
+
+    if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+      jwt = authorizationHeader.substring(7);
+      //если подпись не совпадает с вычисленной, то SignatureException
+      //если подпись некорректная (не парсится) то MalformedJwtException
+      //если подпись истекла по времени,  то ExpiredJwtException
+      username = jwtUtil.extractUsername(jwt);
+    }
+
+
+    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+      String commaSeparatedListOfAuthorities = jwtUtil.extractAuthorities(jwt);
+      List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList(commaSeparatedListOfAuthorities);
+      UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+          new UsernamePasswordAuthenticationToken(
+              username, null, authorities);
+
+      SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+    }
+    chain.doFilter(request, response);
+  }
+
+}
---
+++ src/test/java/ru/sysout/jwtexample/HelloControllerTests.java
@@ -0,0 +1,76 @@
+package ru.sysout.jwtexample;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.sysout.jwt.security.AuthRequest;
+import ru.sysout.jwt.security.AuthResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerTests {
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @Test
+  public void whenGetUser_thenCorrect() {
+
+    AuthResponse authResponse = getAuthHeaderForUser("user", "password");
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Authorization", "Bearer " + authResponse.getJwtToken());
+
+    ResponseEntity<String> response = restTemplate.exchange("/user", HttpMethod.GET, new HttpEntity<>(headers), String.class);
+
+    assertTrue(response.getBody().equals("User"));
+
+  }
+
+  @Test
+  public void whenGetAdmin_thenCorrect() {
+
+    AuthResponse authResponse = getAuthHeaderForUser("admin", "password");
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Authorization", "Bearer " + authResponse.getJwtToken());
+
+    ResponseEntity<String> response = restTemplate.exchange("/admin", HttpMethod.GET, new HttpEntity<>(headers), String.class);
+
+    assertTrue(response.getBody().equals("Admin"));
+
+  }
+
+  @Test
+  public void givenUserCredentials_whenGetAdmin_thenCorrect() {
+
+    AuthResponse authResponse = getAuthHeaderForUser("user", "password");
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Authorization", "Bearer " + authResponse.getJwtToken());
+
+    ResponseEntity<String> response = restTemplate.exchange("/admin", HttpMethod.GET, new HttpEntity<>(headers), String.class);
+
+    assertEquals(response.getStatusCode(), HttpStatus.FORBIDDEN);
+
+  }
+
+  private AuthResponse getAuthHeaderForUser(String name, String password) {
+
+    AuthRequest authRequest = new AuthRequest();
+    authRequest.setName(name);
+    authRequest.setPassword(password);
+    AuthResponse authResponse = restTemplate.postForObject("/authenticate", authRequest, AuthResponse.class);
+
+    return authResponse;
+  }
+
+}
+

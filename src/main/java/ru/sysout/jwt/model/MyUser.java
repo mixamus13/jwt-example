3735e9055d2a31aa39d5c35671404430536a609d
maxim13 <ZLf7XV6s13>
2021-06-15 22:45:04 +0300

    added lombok

--- src/main/java/ru/sysout/jwt/model/MyUser.java
+++ src/main/java/ru/sysout/jwt/model/MyUser.java
@@ -4,8 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Entity
+@Data
+@RequiredArgsConstructor
 public class MyUser {
 
   @Id
@@ -15,48 +19,5 @@ public class MyUser {
   private String password;
   private String position;
   private String role;
-
-  public String getLogin() {
-    return login;
-  }
-
-  public void setLogin(String login) {
-    this.login = login;
-  }
-
-  public String getPosition() {
-    return position;
-  }
-
-  public void setPosition(String position) {
-    this.position = position;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-
-  public String getRole() {
-    return role;
-  }
-
-  public void setRole(String role) {
-    this.role = role;
-  }
-
 
 }

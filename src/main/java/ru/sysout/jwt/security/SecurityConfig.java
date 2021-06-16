7f7da6557f02f4b9c57f3fd74e62787dbda5222b
maxim13 <ZLf7XV6s13>
2021-06-16 20:39:37 +0300

    corrected code-style

--- src/main/java/ru/sysout/jwt/security/SecurityConfig.java
+++ src/main/java/ru/sysout/jwt/security/SecurityConfig.java
@@ -48,7 +48,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable()
+    http
+        .csrf()
+        .disable()
         .authorizeRequests()
         .antMatchers("/authenticate").permitAll()
         .and()
